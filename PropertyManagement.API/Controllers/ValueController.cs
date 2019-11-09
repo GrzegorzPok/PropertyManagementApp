a17cdb2f0e2d96bd5ef681990345a2bc5567a8b5
GrzegorzPok <pokrzywniak.grzegorz@gmail.com>
2019-11-09 11:11:27 +0100

    finished auth api

--- PropertyManagement.API/Controllers/ValueController.cs
+++ PropertyManagement.API/Controllers/ValueController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -10,6 +11,7 @@ using PropertyManagement.API.Data;
 
 namespace PropertyManagement.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -28,6 +30,7 @@ namespace PropertyManagement.API.Controllers
         }
  
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<ActionResult> GetValue(int id)
         {
--- PropertyManagement.API/Data/DataContext.cs
+++ PropertyManagement.API/Data/DataContext.cs
@@ -11,5 +11,6 @@ namespace PropertyManagement.API.Data
         }
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
