56ffb5feb08bc67d4b7e5df29b4f036ade8ab1d6
GrzegorzPok <pokrzywniak.grzegorz@gmail.com>
2019-11-12 20:57:28 +0100

    login and register

---
+++ PropertyManagement-SPA/src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from 'selenium-webdriver/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  registerMode = false;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.registerMode = !this.registerMode;
+  }
+
+  registerToggle() {
+    this.registerMode = true;
+  }
+
+  cancelRegisterMode(registerMode: boolean) {
+    this.registerMode = registerMode;
+  }
+
+}
